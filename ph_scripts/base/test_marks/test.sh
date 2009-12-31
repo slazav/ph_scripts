1f8164cc1a949e5fe3f2a6db17b873e95638fec0
Vladislav Zavjalov <slazav@altlinux.org>
2009-12-31 11:17:06 +0300

    ph_update_www: -m <style> parameter instead of -H

--- ph_scripts/base/test_marks/test.sh
+++ ph_scripts/base/test_marks/test.sh
@@ -9,11 +9,12 @@ t(){
   shift
   [ -s "$base$n.fig" ] || ln -s -- "$base.fig" "$base$n.fig"
   [ -s "$base$n.jpg" ] || ln -s -- "$base.jpg" "$base$n.jpg"
-  rm -f -- "$base${n}_m.jpg"
+  rm -f -- "$base${n}_m.gif"
   time ../ph_update_www "$@" "$base$n.jpg"
 }
 
-t 1
-t 2 -H
+t 1 -m simple_gif
+t 2 -m aa_gif_dark_halo
+t 3 -m aa_gif_halo
 
 
