bd709c4cdca05863f31c41a74779d702bc22fe2c
Vladislav Zavjalov <slazav@altlinux.org>
2010-01-02 12:37:18 +0300

    ph_update_www: update mark styles

--- ph_scripts/base/test_marks/test.sh
+++ ph_scripts/base/test_marks/test.sh
@@ -10,11 +10,13 @@ t(){
   [ -s "$base$n.fig" ] || ln -s -- "$base.fig" "$base$n.fig"
   [ -s "$base$n.jpg" ] || ln -s -- "$base.jpg" "$base$n.jpg"
   rm -f -- "$base${n}_m.gif"
+  echo ">>> ph_update_www $* $base$n.jpg"
   time ../ph_update_www "$@" "$base$n.jpg"
 }
 
 t 1 -m simple_gif
-t 2 -m aa_gif_dark_halo
+t 2 -m aa_gif
 t 3 -m aa_gif_halo
+t 4 -m aa_gif_dark_halo
 
 
